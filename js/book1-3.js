a29d24fb1f3d9f4bf165da5bf2e2372915db8c1e
Traci Ruffner <traci.ruffner@FASTLANEUS.COM>
2017-07-23 21:18:18 -0400

    added more story and a TO BE CONTINUED page

---
+++ js/book1-3.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $('#title').show();
+    $('#dragon2').show();
+    $('#part1').hide();
+    $('#part2').hide();
+    $('#part3').hide();
+    $('#part4').hide(); 
+    $('#choices').hide();
+    $('#choose1').hide();
+    $('#choose2').hide();
+    
+    $('#pg1').click (function(){
+        $('#part1').show();
+        $('#part2').hide();
+        $('#part3').hide();
+        $('#part4').hide(); 
+        $('#dragon2').hide();
+        $('#choices').hide();
+        $('#choose1').hide();
+        $('#choose2').hide();
+    });
+    
+    $('#pg2').click(function(){
+        $('#part1').hide();
+        $('#part2').show();
+        $('#part3').hide();
+        $('#part4').hide(); 
+        $('#dragon2').hide();
+        $('#choices').hide();
+        $('#choose1').hide();
+        $('#choose2').hide();
+      }); 
+    
+    $('#cover').click(function(){
+        $('#title').show();
+        $('#dragon2').show();
+        $('#part1').hide();
+        $('#part2').hide();
+        $('#part3').hide();
+        $('#part4').hide(); 
+        $('#choices').hide();
+        $('#choose1').hide();
+        $('#choose2').hide();
+    });
+    
+     $('#pg3').click(function(){
+        $('#part1').hide();
+        $('#part2').hide();
+        $('#part3').show();
+        $('#part4').hide();  
+        $('#dragon2').hide();
+        $('#choices').hide();
+        $('#choose1').hide();
+        $('#choose2').hide();
+      }); 
+    
+    $('#pg4').click(function(){
+        $('#part1').hide();
+        $('#part2').hide();
+        $('#part3').hide(); 
+        $('#part4').show();
+        $('#dragon2').hide();
+        $('#choices').show();
+        $('#choose1').show();
+        $('#choose2').show();
+    });
+    
+    
+});
